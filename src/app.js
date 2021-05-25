e8fbf19d0491a61b4a2a1f3a93b7ffaebc7bb789
Juan Parra <juandkpa@gmail.com>
2021-05-24 22:34:56 -0500

    start swagger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import contactRouter from './resources/contact';
 import { developmentErrors, productionErrors } from './middleware/error';
 import fileCleaner from './middleware/fileCleaner';
 
 const app = express();
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use(
   cors({
@@ -17,6 +20,7 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 
 app.use('/api/contacts', contactRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.static('public'));
 
